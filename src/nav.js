42b1555eaad916e0fa842798219ad4d20b25550b
Becky Van Vickle <opticalchick@yahoo.com>
2024-01-17 10:49:32 -0600

    finished lab coding and uploaded to github

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        )
+    }
+}
